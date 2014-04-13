1cf37a8cfd521c5c3cb68b9c34ff69692a5cb2e2
Madalin Ignisca <madalin.ignisca@gmail.com>
2014-04-13 22:20:28 +0300

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -17,11 +17,15 @@ done
  
 # Set the environment by loading from the file "environment" in the same directory
 DIR="$( cd "$( dirname $( dirname "$0" ) )" && pwd)"
-source "$DIR/.env"
+# source "$DIR/.env"
+DEPLOY_SOURCE_DIR="out/"
+DEPLOY_DEST_DIR="~/httpdocs"
+DEPLOY_SERVER="athena.webgrid.ro"
+DEPLOY_ACCOUNT="mignisca"
  
 echo "Deploying ${DIR}/${DEPLOY_SOURCE_DIR} to ${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}"
 
 docpad clean 
 docpad generate --env static
 chmod -R og+Xr out
-rsync $NFLAG -rvzp --delete --exclude-from="$DIR/.deployignore" "${DIR}/${DEPLOY_SOURCE_DIR}" "${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}"
+rsync $NFLAG -rvzp --delete --exclude-from="$DIR/.deployignore" "${DIR}/${DEPLOY_SOURCE_DIR}" "${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}" --rsh='ssh -p54876'
